381dfffc05d0e83ee2f8d77b3188f84172e5100d
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-05-20 22:48:28 -0300

    Passando formulário via POST

--- formulario.php
+++ formulario.php
@@ -1,29 +1,19 @@
-<form class="container">
-    <div class="container">
+<form class="container" method="post" action="processa_mensagem.php">
+    <div class="form-group col-md-6">
         <br><br>
-        <div class="form-group col-md-6">
-            <label for="email">Qual o seu endereço de e-mail?</label>
-            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="name@example.com">
-        </div>
+        <label for="email">Qual o seu endereço de e-mail?</label>
+        <input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="name@example.com">
 
-        <div class="form-group col-md-6">
-            <label for="nome">Qual o seu nome?</label>
-            <input type="text" class="form-control" id="nome" placeholder="Digite o seu nome">
-        </div>
+        <label for="nome">Qual o seu nome?</label>
+        <input type="text" class="form-control" name="nome" placeholder="Digite o seu nome">
 
-        <div class="form-group col-md-6">
-            <label for="assunto">Qual o assunto da sua mensagem?</label>
-            <input type="text" class="form-control" id="assunto" placeholder="Digite o assunto">
-        </div>
+        <label for="assunto">Qual o assunto da sua mensagem?</label>
+        <input type="text" class="form-control" name="assunto" placeholder="Digite o assunto">
 
-        <div class="form-group col-md-6">
-            <label for="mensagem">Qual a sua mensagem?</label>
-            <textarea class="form-control" id="mensagem" rows="3"></textarea>
-        </div>
+        <label for="mensagem">Qual a sua mensagem?</label>
+        <textarea class="form-control" name="mensagem" rows="3"></textarea>
 
-        <div class="container">
-            <input type="submit" value="Enviar mensagem">
-        </div>
+        <input type="submit" value="Enviar mensagem">
 
         <br><br>
 
---
+++ processa_mensagem.php
@@ -0,0 +1,18 @@
+<?php
+$email = $_POST['email'];
+$nome = $_POST['nome'];
+$assunto = $_POST['assunto'];
+$mensagem = $_POST['mensagem'];
+
+include 'header.php';
+?>
+
+<div class="container">
+
+
+</div>
+
+
+<?php
+include 'footer.php';
+?>
